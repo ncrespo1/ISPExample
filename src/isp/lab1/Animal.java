4131b159a9e9e49439ebd6163cedba46e04ac542
Nic <ncrespo@my.wctc.edu>
2018-04-13 11:54:26 -0500

    Initial commit

---
+++ src/isp/lab1/Animal.java
@@ -0,0 +1,11 @@
+package isp.lab1;
+
+public interface Animal {
+    public void talk(String s);
+    public void run(int distance);
+    public void jump();
+    public void hike();
+    public void swim();
+    public void trip();
+
+}
---
+++ src/isp/lab1/Vehicle.java
@@ -0,0 +1,10 @@
+package isp.lab1;
+
+public interface Vehicle {
+
+    public void start();
+    public void drive(int distance);
+    public void stopByBreaking();
+
+}
+
